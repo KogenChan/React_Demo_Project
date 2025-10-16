398786bea04ddb8167d242bc7f8bee77f8e4790c
Evelyn <narunaga@live.it>
2025-10-16 20:00:47 +0200

    fixed typo in footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,11 +3,15 @@ import routes from '../routing/routes.min';
 
 export default function Footer() {
    return (
-      <div className='text-center text-base-content p-3 bg-base-200'}}>
-        © 2025 Copyright:
-        <Link className='text-base-content' to={routes.home}>
-          John Doe
-        </Link>
+      <div className='text-xs text-center text-base-content p-5 bg-base-200'>
+         © 2025 Copyright:
+         <Link 
+            className='pl-1 text-base-content hover:underline' 
+            to={routes.home}
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+         >
+            John Doe
+         </Link>
       </div>
    );
 };
